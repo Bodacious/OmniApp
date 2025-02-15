138bd115455c5b8def47ce6ebd698e35dd8144e8
Bodacious <gavin@gavinmorrice.com>
2025-02-15 23:08:47 +0000

    Add some tests for list CRUD actions

--- sinatra_app/test/main_test.rb
+++ sinatra_app/test/main_test.rb
@@ -16,8 +16,8 @@ class MainTest < Minitest::Test
 
     fill_in :"list[name]", with: "list-name"
     click_on "Save"
-    
-    assert page.has_content?("list-name")
+
+    assert_content("list-name")
 
     assert_equal current_path, '/lists/list-name'
 
@@ -26,6 +26,40 @@ class MainTest < Minitest::Test
     fill_in :"list_item[summary]", with: "list-item-summary"
     click_on "Save"
 
-    assert page.has_content?("list-item-summary")
+    assert_content("list-item-summary")
+  end
+
+  def test_user_creates_new_list_and_then_deletes_it
+    visit "/"
+    click_on "New list"
+
+    fill_in :"list[name]", with: "list-name"
+    click_on "Save"
+
+    assert_content("list-name")
+    assert_current_path '/lists/list-name'
+
+    click_on 'Delete list'
+
+    assert_current_path '/'
+    refute_content 'list-name'
+  end
+
+  def test_user_creates_new_list_item_and_then_deletes_it
+    visit "/"
+    click_on "New list"
+
+    fill_in :"list[name]", with: "list-name"
+    click_on "Save"
+
+    fill_in :"list_item[summary]", with: "list-item-summary"
+    click_on 'Save'
+
+    assert_content 'list-item-summary'
+    assert_current_path '/lists/list-name'
+
+    click_on 'Remove'
+    refute_content 'list-item-summary'
+    assert_current_path '/lists/list-name'
   end
 end
---
+++ sinatra_app/test/support/assertions.rb
@@ -0,0 +1,21 @@
+module Assertions
+  ##
+  # Assert that the page has given content (text)
+  # @param [String] content The content to test for
+  # @param [String] message A message to display if content not found
+  # @return [void]
+  def assert_content(content, message = nil)
+    message ||= "Expected page to have content '#{content}' but did not"
+    assert page.has_content?(content.to_s), message
+  end
+
+  def refute_content(content, message = nil)
+    message ||= "Expected page to not have content '#{content}' but did"
+    refute page.has_content?(content.to_s), message
+  end
+
+  def assert_current_path(path, message = nil)
+    message ||= "Expected current path to be '#{content}' but #{current_path}"
+    assert_equal path, current_path, message
+  end
+end
