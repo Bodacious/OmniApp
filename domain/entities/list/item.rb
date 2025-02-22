de86adc3116c3fdbfd17f8ebae7d8aa732de51bb
Bodacious <gavin@gavinmorrice.com>
2025-02-22 22:28:12 +0000

    Hide done in List::Item
    Keep state hidden

--- domain/entities/list/item.rb
+++ domain/entities/list/item.rb
@@ -4,9 +4,10 @@ class List
   ##
   # A List::Item represents one item on a list that can be marked as "done" when complete.
   class Item
-    attr_accessor :summary
-    attr_writer :done
+    attr_reader :summary
 
+    attr_writer :done
+    private :done=
     def initialize(**attributes)
       @summary = attributes[:summary]
       @done = attributes[:done]
