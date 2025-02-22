032a875495ef71fbb405cd6ecbe3639900867341
Bodacious <gavin@gavinmorrice.com>
2025-02-22 22:45:39 +0000

    Move List::Item into its own set class

--- domain/entities/list.rb
+++ domain/entities/list.rb
@@ -1,18 +1,46 @@
 # frozen_string_literal: true
-
+require 'forwardable'
 require_relative "list/item"
 ##
 # A List is a single place to store a group of related tasks or reminders
 class List
+  # Stores all of the items in a given List
+  class ListItemSet
+    extend Forwardable
+    attr_reader :set
+
+    def_delegator :set, :delete, :remove
+    def_delegators :set, :add, :include?, :empty?
+
+    def initialize
+      @set = Set.new
+    end
+  end
+
+  ##
+  # Name of the list
+  # @return [String]
   attr_reader :name
+
+  ##
+  # List of items in this List
+  # @return [ListItemSet]
   attr_accessor :items
 
   def initialize(**attributes)
     @name = attributes[:name]
-    @items = Set.new
+    @items = ListItemSet.new
   end
 
+  # Add an item to this list
+  # @param [List::Item] item
   def add_item(item)
     items.add(item)
+  end
+
+  # Remove an item from this list
+  # @param [List::Item] item
+  def remove_item(item)
+    items.remove(item)
   end
 end
--- test/domain/entities/list_test.rb
+++ test/domain/entities/list_test.rb
@@ -10,12 +10,29 @@ class ListTest < Minitest::Test
 
   def test_list_has_many_list_items
     list = List.new
-    list_item = List::Item.new(summary: "list-item-summary")
 
     assert_empty list.items
+  end
+
+  def test_list_can_accept_an_item
+    list = List.new
+    list_item = List::Item.new(summary: "list-item-summary")
 
     list.add_item(list_item)
 
     assert_includes list.items, list_item
   end
+
+  def test_list_can_remove_an_item
+    list = List.new
+    list_item = List::Item.new(summary: "list-item-summary")
+
+    list.add_item(list_item)
+    assert_includes list.items, list_item
+
+    list.remove_item(list_item)
+
+    assert_empty list.items
+  end
+
 end
