83bf0ea02a0c633786ab6d3e0279813f51cbba0a
Bodacious <gavin@gavinmorrice.com>
2025-02-22 22:52:48 +0000

    Make list item repeatable

--- domain/entities/list/item.rb
+++ domain/entities/list/item.rb
@@ -8,15 +8,25 @@ class List
 
     attr_writer :done
     private :done=
+
+    attr_writer :repeatable
+    private :repeatable=
     def initialize(**attributes)
       @summary = attributes[:summary]
       @done = attributes[:done]
+      @repeatable = attributes.fetch(:repeatable, false)
     end
 
     def done? = @done
 
+    def repeatable? = @repeatable
+
     def toggle_done
       self.done = !done?
+    end
+
+    def toggle_repeatable
+      self.repeatable = !repeatable?
     end
   end
 end
---
+++ test/domain/entities/list/item_test.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "domain/entities/list/item"
+
+class List
+  class ListItemTest < Minitest::Test
+    def test_it_has_a_summary
+      list_item = List::Item.new(summary: "list-item-summary")
+
+      assert_equal "list-item-summary", list_item.summary
+    end
+
+    def test_it_is_not_done_by_default
+      list_item = List::Item.new
+
+      refute_predicate list_item, :done?
+    end
+
+    def test_toggle_done_toggles_done_value
+      list_item = List::Item.new
+
+      refute_predicate list_item, :done?
+      list_item.toggle_done
+
+      assert_predicate list_item, :done?
+      list_item.toggle_done
+
+      refute_predicate list_item, :done?
+    end
+
+    def test_list_items_can_be_repeatable
+      list_item = List::Item.new repeatable: true
+      assert_predicate list_item, :repeatable?
+
+      list_item = List::Item.new repeatable: false
+      refute_predicate list_item, :repeatable?
+    end
+
+    def test_toggle_repeatable_changes_the_repeatable_value
+      list_item = List::Item.new
+      refute_predicate list_item, :repeatable?
+
+      list_item.toggle_repeatable
+
+      assert_predicate list_item, :repeatable?
+    end
+  end
+end
